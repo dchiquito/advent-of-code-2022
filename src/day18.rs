863c81ecabe6bff3043679791626fa6dbd1a7f6b
Daniel Chiquito <daniel.chiquito@gmail.com>
2022-12-18 12:59:34 -0500

    Format

--- src/day18.rs
+++ src/day18.rs
@@ -8,31 +8,40 @@ type Volume = HashSet<Pos>;
 
 fn read_input() -> Volume {
     let lines = advent::read_input(18);
-    lines.iter().map(|line| {
-        let mut arr = line.split(",");
-        (arr.next().unwrap().parse().unwrap(), arr.next().unwrap().parse().unwrap(), arr.next().unwrap().parse().unwrap())
-    }).collect()
+    lines
+        .iter()
+        .map(|line| {
+            let mut arr = line.split(",");
+            (
+                arr.next().unwrap().parse().unwrap(),
+                arr.next().unwrap().parse().unwrap(),
+                arr.next().unwrap().parse().unwrap(),
+            )
+        })
+        .collect()
 }
 
 fn adjacents((x, y, z): &Pos) -> Vec<Pos> {
     let (x, y, z) = (*x, *y, *z);
     vec![
-        (x-1, y, z),
-        (x+1, y, z),
-        (x, y-1, z),
-        (x, y+1, z),
-        (x, y, z-1),
-        (x, y, z+1),
+        (x - 1, y, z),
+        (x + 1, y, z),
+        (x, y - 1, z),
+        (x, y + 1, z),
+        (x, y, z - 1),
+        (x, y, z + 1),
     ]
 }
 
 fn surface_area(voxels: &Volume) -> u32 {
     voxels
         .iter()
-        .map(|pos| 
-             adjacents(&pos)
-             .iter()
-             .filter(|adj| !voxels.contains(adj)).count() as u32)
+        .map(|pos| {
+            adjacents(&pos)
+                .iter()
+                .filter(|adj| !voxels.contains(adj))
+                .count() as u32
+        })
         .fold(0, u32::add)
 }
 pub fn solve_1() -> u32 {
@@ -53,11 +62,11 @@ fn max_pos(volume: &Volume) -> Pos {
 fn invert(volume: &Volume) -> Volume {
     let mut inversion = HashSet::new();
     let (mx, my, mz) = max_pos(volume);
-    for x in 0..mx+1 {
-        for y in 0..my+1 {
-            for z in 0..mz+1 {
-                if !volume.contains(&(x,y,z)) {
-                    inversion.insert((x,y,z));
+    for x in 0..mx + 1 {
+        for y in 0..my + 1 {
+            for z in 0..mz + 1 {
+                if !volume.contains(&(x, y, z)) {
+                    inversion.insert((x, y, z));
                 }
             }
         }
@@ -78,7 +87,7 @@ fn solve_2() -> u32 {
     let voxels = read_input();
     let inversion = invert(&voxels);
     let mut outside = HashSet::new();
-    find_subset(&inversion, &(0,0,0), &mut outside);
+    find_subset(&inversion, &(0, 0, 0), &mut outside);
     let filled_voxels = invert(&outside);
     surface_area(&filled_voxels)
 }
